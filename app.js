49bc18b714a4ba190641684d03e8740b7e165f3d
cywen <18205055638@163.com>
2021-04-16 10:57:20 +0800

    去除 app.js 默认逻辑

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    this.getIconFont();
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  },
-  getIconFont: () => {
-    wx.loadFontFace({
-      global: true,
-      family: "iconfont",
-      source: "url('https://oss.flywen.top/iconfont.ttf')",
-      success: (res) => console.log(res.status),
-      fail: (res) => console.log(res.status)
-    })
   }
 })
